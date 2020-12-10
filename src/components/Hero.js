4e5fa0e3591c55c88fcc3a8d490663c73b2bba44
0sorojo <redrootproductions@gmail.com>
2020-12-10 10:37:49 -0500

    changed hero styling to a useeffect conditional

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context';
+import light from '../images/sample-R2-svgHeroYellowv2.svg';
+import dark from '../images/sample-R2-svgHeroBluev1.svg';
 
 const Hero = () => {
+  const { theme } = useGlobalContext();
+  const [themeStyle, setThemeStyle] = useState({});
+
+  const heroStyle = () => {
+    if (theme === 'light-theme') {
+      setThemeStyle({
+        background: `url(${light}) center center`,
+      });
+    }
+    if (theme === 'dark-theme') {
+      setThemeStyle({
+        background: `url(${dark}) center center`,
+      });
+    }
+    return themeStyle;
+  };
+
+  useEffect(() => {
+    heroStyle();
+    console.log('i work');
+  }, [theme]);
+
   return (
-    <section className='hero'>
+    <section className='hero' style={themeStyle}>
       <div className='hero-center'>
         <article className='hero-info'>
           <h1>A Full-Stack Engineer</h1>
